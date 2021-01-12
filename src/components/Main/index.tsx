aa086091c1e35760fbbbc05fb5ef536d60828f85
anabeatriz <annabhiaaraujo.silva@gmail.com>
2021-01-11 22:36:05 -0300

    Adiciona componentes principais: Contaier, Wrapper, Main

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { 
+    Container, 
+    Header, 
+    BackIcon, 
+    ProfileInfo, 
+    BottomMenu, 
+    HomeIcon, 
+    SearchIcon,
+    BellIcon, 
+    EmailIcon } from '../Main/styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Ana Beatriz</strong>
+          <span>530 Tweets</span>
+        </ProfileInfo>
+      </Header>
+      {/* <ProfilePage/> */}
+      
+      <BottomMenu>
+
+          <HomeIcon className="active" />
+          <SearchIcon />
+          <BellIcon />
+          <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+};
+
+export default Main;
